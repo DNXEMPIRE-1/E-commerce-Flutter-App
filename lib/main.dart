956d99341aa5c595b82ae250a38f8b248af6c60b
“Dennis <dennisnzioki019@gmail.com>
2024-02-21 15:44:08 +0300

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/screens/splash/splash_screen.dart';
+
+import 'routes.dart';
+import 'theme.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'The Flutter Way - Template',
+      theme: AppTheme.lightTheme(context),
+      initialRoute: SplashScreen.routeName,
+      routes: routes,
+    );
+  }
+}
